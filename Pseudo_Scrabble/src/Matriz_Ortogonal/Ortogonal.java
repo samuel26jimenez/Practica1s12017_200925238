e41f91750823029326d479ce075d6152af1580eb
Samuel <Samuel@192.168.8.100>
2017-02-15 21:36:26 -0600

    Implementacion de Matriz Ortogonal, Carga de lectura archivo xml y masiva para las estructuras..

--- Pseudo_Scrabble/src/Matriz_Ortogonal/Ortogonal.java
+++ Pseudo_Scrabble/src/Matriz_Ortogonal/Ortogonal.java
@@ -10,5 +10,39 @@ package Matriz_Ortogonal;
  * @author Samuel
  */
 public class Ortogonal {
+    public char le;
+    public int x , y, premio;
+   
+    public Ortogonal ar;
+    public Ortogonal ab;
+    public Ortogonal iz;
+    public Ortogonal der;
     
+    
+    public Ortogonal(int X, int Y){ 
+        // Punteros
+        this.x = X;
+        this.y = Y;
+        this.ar = null;
+        this.ab = null;
+        this.iz = null;
+        this.der = null;
+    }
+    
+    public void set(char let){
+        this.le = let;
+    }
+    
+    public char get(){
+        return le;
+    }
+    
+    public void set_Premio(int pre){
+        this.premio = pre;        
+    }
+    
+    public int get_Premio(){
+        return premio;
+    }    
 }
+
